e6aa96c3c673cdc71d6975da6cb399a2b683f96c
nikrich <jannik811@gmail.com>
2025-09-10 17:51:40 +0200

    Added CI CD

--- HungryGhostLimiter/tests/RunAllTests.cpp
+++ HungryGhostLimiter/tests/RunAllTests.cpp
@@ -1,6 +1,8 @@
 #include <juce_core/juce_core.h>
+#include <juce_gui_basics/juce_gui_basics.h>
 
 int main() {
+    juce::ScopedJuceInitialiser_GUI gui;
     juce::UnitTestRunner r;
     r.runAllTests();
     return 0;
